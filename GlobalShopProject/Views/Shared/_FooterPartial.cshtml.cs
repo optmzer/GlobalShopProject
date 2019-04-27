29791673605df764e7bd713fecbc257cafd52485
Alexander Frolov <big_air2409@mail.ru>
2019-04-27 14:31:35 +1200

    Added font-awesome 4.7.0. Created _FooterPartial

---
+++ GlobalShopProject/Views/Shared/_FooterPartial.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GlobalShopProject.Views.Shared
+{
+    public class _FooterPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
